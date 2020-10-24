d79a08e331a43c118dfa076f592d5a6837dcf6b2
Gustavo Azevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2020-10-24 20:22:19 -0300

    scoreName refactoring

--- tennis1.js
+++ tennis1.js
@@ -1,23 +1,16 @@
 'use strict';
 
 function scoreName(points) {
-    let score = ""
     switch (points) {
         case 0:
-            score += "Love";
-            break;
+            return "Love";
         case 1:
-            score += "Fifteen";
-            break;
+            return "Fifteen";
         case 2:
-            score += "Thirty";
-            break;
+            return "Thirty";
         case 3:
-            score += "Forty";
-            break;
+            return "Forty";
     }
-    return score
-    
 }
 
 function getScore(m_score1, m_score2) {
@@ -26,7 +19,7 @@ function getScore(m_score1, m_score2) {
     var score = "";
     if (diference === 0) {
         score = m_score1 > 2 ? "Deuce" : scoreName(m_score1) + "-All"
-        
+
     } else if (m_score1 >= 4 || m_score2 >= 4) {
         score = (diference === 1) ? "Advantage player" : "Win for player"
         score += m_score1 - m_score2 > 0 ? "1" : "2"
