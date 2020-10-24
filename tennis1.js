4d897225a0659b3dab8c77deac457261f8a3a33c
Gustavo Azevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2020-10-24 20:27:18 -0300

    Final!

--- tennis1.js
+++ tennis1.js
@@ -16,18 +16,15 @@ function scoreName(points) {
 function getScore(m_score1, m_score2) {
     const diference = m_score1 - m_score2 > 0 ? m_score1 - m_score2 : m_score2 - m_score1;
 
-    var score = "";
     if (diference === 0) {
-        score = m_score1 > 2 ? "Deuce" : scoreName(m_score1) + "-All"
+        return m_score1 > 2 ? "Deuce" : scoreName(m_score1) + "-All"
 
     } else if (m_score1 >= 4 || m_score2 >= 4) {
-        score = (diference === 1) ? "Advantage player" : "Win for player"
-        score += m_score1 - m_score2 > 0 ? "1" : "2"
+        const score = (diference === 1) ? "Advantage player" : "Win for player"
+        return  m_score1 - m_score2 > 0 ? score + "1" : score + "2"
 
-    } else {
-        score = scoreName(m_score1) + "-" + scoreName(m_score2)
-    }
-    return score;
+    } 
+    return scoreName(m_score1) + "-" + scoreName(m_score2)
 }
 
 module.exports = getScore;
