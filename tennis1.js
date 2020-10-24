528eceb87a17c473b560466f69a92be07226a746
Gustavo Azevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2020-10-24 20:11:27 -0300

    On going scores

--- tennis1.js
+++ tennis1.js
@@ -45,28 +45,7 @@ function getScore(m_score1, m_score2) {
         score += m_score1 - m_score2 > 0 ? "1" : "2"
 
     } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) {tempScore = m_score1;}
-            else {
-                score += "-";
-                tempScore = m_score2;
-            }
-            score += scoreName(tempScore)
-            // switch (tempScore) {
-            //     case 0:
-            //         score += "Love";
-            //         break;
-            //     case 1:
-            //         score += "Fifteen";
-            //         break;
-            //     case 2:
-            //         score += "Thirty";
-            //         break;
-            //     case 3:
-            //         score += "Forty";
-            //         break;
-            // }
-        }
+        score = scoreName(m_score1) + "-" + scoreName(m_score2)
     }
     return score;
 }
