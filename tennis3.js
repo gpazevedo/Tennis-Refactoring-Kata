ddafcfd525fed0d791fe5a84f848ab1e44cebeb0
Gustavo Azevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2020-10-25 19:49:37 -0300

    3: diference

--- tennis3.js
+++ tennis3.js
@@ -1,22 +1,21 @@
-'use strict';
+"use strict";
 
 function getScore(p1, p2) {
-    var s;
-    let p1N = "player1";
-    let p2N = "player2";
-    if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6)) {
-        var p = ["Love", "Fifteen", "Thirty", "Forty"];
-        s = p[p1];
-        return (p1 === p2) ? s + "-All" : s + "-" + p[p2];
-    } else {
-        if (p1 === p2) {
-            return "Deuce";
-        }
-        s = p1 > p2 ? p1N : p2N;
-        return ((p1 - p2) * (p1 - p2) === 1) ? "Advantage " + s : "Win for " + s;
+  const diference = p1 - p2 > 0 ? p1 - p2 : p2 - p1;
+  var s;
+  let p1N = "player1";
+  let p2N = "player2";
+  if (p1 < 4 && p2 < 4 && p1 + p2 < 6) {
+    var p = ["Love", "Fifteen", "Thirty", "Forty"];
+    s = p[p1];
+    return diference === 0 ? p[p1] + "-All" : p[p1] + "-" + p[p2];
+  } else {
+    if (p1 === p2) {
+      return "Deuce";
     }
+    s = p1 > p2 ? p1N : p2N;
+    return diference === 1 ? "Advantage " + s : "Win for " + s;
+  }
 }
 
-
 module.exports = getScore;
-
