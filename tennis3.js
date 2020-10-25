c9268e37a7d4935a101147f167ba6c5e8eef8b41
Gustavo Azevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2020-10-25 20:09:47 -0300

    3: Final

--- tennis3.js
+++ tennis3.js
@@ -2,20 +2,20 @@
 
 function getScore(p1, p2) {
   const diference = p1 - p2 > 0 ? p1 - p2 : p2 - p1;
-  var s;
-  let p1N = "player1";
-  let p2N = "player2";
+  const points = ["Love", "Fifteen", "Thirty", "Forty"];
+
   if (p1 < 4 && p2 < 4 && p1 + p2 < 6) {
-    var p = ["Love", "Fifteen", "Thirty", "Forty"];
-    s = p[p1];
-    return diference === 0 ? p[p1] + "-All" : p[p1] + "-" + p[p2];
-  } else {
-    if (p1 === p2) {
-      return "Deuce";
-    }
-    s = p1 > p2 ? p1N : p2N;
-    return diference === 1 ? "Advantage " + s : "Win for " + s;
+    return diference === 0
+      ? points[p1] + "-All"
+      : points[p1] + "-" + points[p2];
   }
+  if (diference === 0) {
+    return "Deuce";
+  }
+  return (
+    (diference === 1 ? "Advantage player" : "Win for player") +
+    (p1 > p2 ? "1" : "2")
+  );
 }
 
 module.exports = getScore;
