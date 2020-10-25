30a86968816dda9645692adac5cb42b7336a940d
Gustavo Azevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2020-10-25 19:10:20 -0300

    First games

--- tennis2.js
+++ tennis2.js
@@ -18,17 +18,17 @@ function getScore(P1point, P2point) {
   const diference =
     P1point - P2point > 0 ? P1point - P2point : P2point - P1point;
 
+  if (P1point >= 3 && P2point >= 3 && diference === 1) {
+    return "Advantage player" + (P1point - P2point > 0 ? "1" : "2");
+  }
+
   if ((P1point >= 4 || P2point >= 4) && diference >= 2) {
     return "Win for player" + (P1point - P2point > 0 ? "1" : "2");
   }
 
-  if (P1point >= 3 && P2point >= 3 && diference >= 1) {
-    return "Advantage player" + (P1point - P2point > 0 ? "1" : "2");
-  }
-
   if (diference === 0) {
     score = P1point < 3 ? scoreName(P1point) + "-All" : "Deuce";
-  } else if (P1point < 4 || P2point < 4) {
+  } else {
     score = scoreName(P1point) + "-" + scoreName(P2point);
   }
 
